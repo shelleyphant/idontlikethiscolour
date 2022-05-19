69073bd4e51294634bb81cf2bee4b28929109e99
Shelley Bassett <m.bassett@y7mail.com>
2022-05-20 07:32:07 +1000

    Removed unused analytics tracking

--- index.php
+++ index.php
@@ -1,47 +1,42 @@
 <!DOCTYPE html>
 <html>
-	<head>
-		<link rel="stylesheet" type="text/css" href="style.css" />
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.0.0/jquery.min.js"></script>
-		<script src="script.js" charset="utf-8"></script>
-		<title>I Don't Like This Colour</title>
-		<script>
-			window.dataLayer = window.dataLayer || [];
-			function gtag(){dataLayer.push(arguments);}
-			gtag('js', new Date());
 
-			gtag('config', 'UA-58048087-1');
-		</script>
-	</head>
-	
-	<body>
-		<div class="container">
-				<a href="#" class="button" onclick="colorgen()">
-					<h1>I Don't Like This Colour</h1>
-				</a>
-		</div>
-	</body>
-  </html>  
+<head>
+    <link rel="stylesheet" type="text/css" href="style.css" />
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.0.0/jquery.min.js"></script>
+    <script src="script.js" charset="utf-8"></script>
+    <title>I Don't Like This Colour</title>
+</head>
+
+<body>
+    <div class="container">
+        <a href="#" class="button" onclick="colorgen()">
+            <h1>I Don't Like This Colour</h1>
+        </a>
+    </div>
+</body>
+
+</html>
 
 
 
-<?php  
-$fp = fopen("counter.txt", "r"); 
-$count = fread($fp, 1024); 
-fclose($fp); 
+<?php
+$fp = fopen("counter.txt", "r");
+$count = fread($fp, 1024);
+fclose($fp);
 
 // Add 1 to the existing count 
-$count = $count + 1; 
+$count = $count + 1;
 
 // Display the number of hits 
 // If you don't want to display it, comment out this line 
 //echo "<p>Page views:" . $count . "</p>"; 
 
 // Reopen the file and erase the contents 
-$fp = fopen("counter.txt", "w"); 
+$fp = fopen("counter.txt", "w");
 // Write the new count to the file 
-fwrite($fp, $count); 
+fwrite($fp, $count);
 // Close the file 
-fclose($fp); 
+fclose($fp);
 
-?> 
+?>
